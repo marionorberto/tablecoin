0706bc15a81d6c81cf282142da9e06daa225dfeb
marionorberto <marionorberto2018@gmail.com>
2024-08-17 01:14:33 +0100

    adding features to the incomes page

--- src/app/components/incomes/incomes.component.ts
+++ src/app/components/incomes/incomes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-incomes',
@@ -8,5 +9,107 @@ import { Component } from '@angular/core';
   styleUrl: './incomes.component.css'
 })
 export class IncomesComponent {
+
+  chartIncomeBySource: any;
+  chartIncomeByCategory: any;
+  chartIncomeTrends: any;
+
+
+  ngOnInit(){
+    this.createChart();
+  }
+
+  createChart(){
+    this.chartIncomeBySource = new Chart('chart-income-by-source', {
+      type: 'pie',
+      data: {
+        labels: ['January','February','March', 'May', 'April', 'June'],
+        datasets: [
+          {
+            label: 'Incomes',
+            data: ['400', '700', '2500', '300', '3500', '1000'],
+            backgroundColor: '#fef9c3',
+            borderColor: '#DAA520',
+          },
+          {
+            label: 'Expenses',
+            data: ['500','1000','1500','3000','3000','3000' ],
+            backgroundColor: '#cbd5e1',
+            borderColor: '#475569',
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5,
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+      }
+
+    });
+
+    this.chartIncomeByCategory = new Chart('chart-income-by-category', {
+      type: 'bar',
+      data: {
+        labels: ['January','February','March', 'May', 'April', 'June'],
+        datasets: [
+          {
+            label: 'Incomes',
+            data: ['400', '700', '2500', '300', '3500', '1000'],
+            backgroundColor: '#fef9c3',
+            borderColor: '#DAA520',
+          },
+          {
+            label: 'Expenses',
+            data: ['500','1000','1500','3000','3000','3000' ],
+            backgroundColor: '#cbd5e1',
+            borderColor: '#475569',
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5,
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+      }
+
+    });
+
+    this.chartIncomeTrends = new Chart('chart-income-trends', {
+      type: 'line',
+      data: {
+        labels: ['January','February','March', 'May', 'April', 'June'],
+        datasets: [
+          {
+            label: 'Incomes',
+            data: ['400', '700', '2500', '300', '3500', '1000'],
+            backgroundColor: '#fef9c3',
+            borderColor: '#DAA520',
+          },
+          {
+            label: 'Expenses',
+            data: ['500','1000','1500','3000','3000','3000' ],
+            backgroundColor: '#cbd5e1',
+            borderColor: '#475569',
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5,
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+      }
+
+    });
+  }
+
 
 }
